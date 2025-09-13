0121d234656d3d8357a004aa7938c2d48e5a9f45
Storozheva-dev <storozheva.business@gmail.com>
2025-09-13 16:07:09 +0200

    Update OpenAPI docs: add deployed server, 401 and 400 responses, make _id required

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -23,5 +23,5 @@ const fileFilter = (_req, file, cb) => {
 export const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024, files: 1 }, // 5MB
+  limits: { fileSize: 5 * 1024 * 1024, files: 1 }, 
 });
